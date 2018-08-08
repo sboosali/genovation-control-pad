f3c9f0fee736f13596944ca8faf5f50784302721
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-08 10:29:52 -0700

    generic-lens

--- sources/Prelude_genovation_control_pad.hs
+++ sources/Prelude_genovation_control_pad.hs
@@ -20,3 +20,8 @@ import "enumerate" Enumerate as X
 import "text" Data.Text as X (Text)
 
 ----------------------------------------
+
+import "generic-lens" Data.Generics.Product as X hiding (IsList,list)
+import "generic-lens" Data.Generics.Sum     as X
+
+----------------------------------------
