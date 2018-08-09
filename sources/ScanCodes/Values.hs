be423e2d879458fe772fa1735f0c41d6984f32fd
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-09 02:51:02 -0700

    [wip]

--- sources/ScanCodes/Values.hs
+++ sources/ScanCodes/Values.hs
@@ -27,12 +27,208 @@ import ScanCodes.Types
 
 -}
 describeKey :: Key -> KeyDescription
-describeKey = _
+describeKey = \case
 
---------------------------------------------------
+  Key_Backtick            -> _
 
-{-|
+  Key_1                   -> _
 
--}
+  Key_2                   -> _
+
+  Key_3                   -> _
+
+  Key_4                   -> _
+
+  Key_5                   -> _
+
+  Key_6                   -> _
+
+  Key_7                   -> _
+
+  Key_8                   -> _
+
+  Key_9                   -> _
+
+  Key_0                   -> _
+
+  Key_Hyphen              -> _
+
+  Key_Equals              -> _
+
+  Key_Backspace           -> _
+
+  Key_Tab                 -> _
+
+  Key_Q                   -> _
+
+  Key_W                   -> _
+
+  Key_E                   -> _
+
+  Key_R                   -> _
+
+  Key_T                   -> _
+
+  Key_Y                   -> _
+
+  Key_U                   -> _
+
+  Key_I                   -> _
+
+  Key_O                   -> _
+
+  Key_P                   -> _
+
+  Key_OpenSquareBracket   -> _
+
+  Key_ClosedSquareBracket -> _
+
+  Key_CapsLock            -> _
+
+  Key_A                   -> _
+
+  Key_S                   -> _
+
+  Key_D                   -> _
+
+  Key_F                   -> _
+
+  Key_G                   -> _
+
+  Key_H                   -> _
+
+  Key_J                   -> _
+
+  Key_K                   -> _
+
+  Key_L                   -> _
+
+  Key_Semicolon           -> _
+
+  Key_Apostrophe          -> _
+
+  Key_Enter               -> _
+
+  Key_Left_Shift          -> _
+
+  Key_Z                   -> _
+
+  Key_X                   -> _
+
+  Key_C                   -> _
+
+  Key_V                   -> _
+
+  Key_B                   -> _
+
+  Key_N                   -> _
+
+  Key_M                   -> _
+
+  Key_Comma               -> _
+
+  Key_Period              -> _
+
+  Key_Slash               -> _
+
+  Key_Right_Shift         -> _
+
+  Key_Left_Ctrl           -> _
+  
+  Key_Left_Alt            -> _
+
+  Key_Spacebar            -> _
+
+  Key_Right_Alt           -> _
+
+  Key_Right_Ctrl          -> _
+
+  Key_Insert              -> _
+
+  Key_Delete              -> _
+
+  Key_ArrowLeft           -> _
+
+  Key_Home                -> _
+
+  Key_End                 -> _
+
+  Key_ArrowUp             -> _
+
+  Key_ArrowDown           -> _
+
+  Key_PageUp              -> _
+
+  Key_PageDown            -> _
+
+  Key_ArrowRight          -> _
+
+  Key_NumLock             -> _
+
+  Key_KeyPad_7            -> _
+
+  Key_KeyPad_4            -> _
+
+  Key_KeyPad_1            -> _
+
+  Key_KeyPad_Divide       -> _
+
+  Key_KeyPad_8            -> _
+
+  Key_KeyPad_5            -> _
+
+  Key_KeyPad_2            -> _
+
+  Key_KeyPad_0            -> _
+
+  Key_KeyPad_Multiply     -> _
+
+  Key_KeyPad_9            -> _
+
+  Key_KeyPad_6            -> _
+
+  Key_KeyPad_3            -> _
+
+  Key_KeyPad_Decimal      -> _
+
+  Key_KeyPad_Subtract     -> _
+
+  Key_KeyPad_Add          -> _
+
+  Key_KeyPad_Enter        -> _
+
+  Key_Esc                 -> _
+
+  Key_F1                  -> _
+
+  Key_F2                  -> _
+
+  Key_F3                  -> _
+
+  Key_F4                  -> _
+
+  Key_F5                  -> _
+
+  Key_F6                  -> _
+
+  Key_F7                  -> _
+
+  Key_F8                  -> _
+
+  Key_F9                  -> _
+
+  Key_F10                 -> _
+
+  Key_F11                 -> _
+
+  Key_F12                 -> _
+
+  Key_PrintScreen         -> _
+
+  Key_ScrollLock          -> _
+
+  Key_PauseBreak          -> _
+
+  Key_Backslash           -> _
 
 --------------------------------------------------
