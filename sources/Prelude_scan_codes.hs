17a17a77dc338a9710d6f5015aaee5d1ec339d6a
Spiros Boosalis <SamBoosalis@gmail.com>
2018-08-08 17:27:21 -0700

    module ScanCodes.Types

---
+++ sources/Prelude_scan_codes.hs
@@ -0,0 +1,20 @@
+----------------------------------------
+
+module Prelude_scan_codes
+ ( module X
+ ) where
+
+----------------------------------------
+
+import "spiros" Prelude.Spiros
+
+----------------------------------------
+
+import "enumerate" Enumerate as X
+
+----------------------------------------
+
+import "generic-lens" Data.Generics.Product as X hiding (IsList,list)
+import "generic-lens" Data.Generics.Sum     as X
+
+----------------------------------------
